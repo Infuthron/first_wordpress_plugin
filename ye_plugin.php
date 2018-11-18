3bb75916fa98d15c8508c83800b1526b3f39b631
Infuthron <34552317+Infuthron@users.noreply.github.com>
2018-11-18 21:02:38 +0100

    Add files via upload

---
+++ ye_plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package  ye_plugin
+ */
+/*
+Plugin Name: ye_plugin
+Plugin URI: http://ye_plugin.com
+Description: Ay yo, diz be plugin. Diz a good plugin.
+Version: 1.0.0
+Author: Nunya Buznez
+Author URI: http://nunya.com
+License: GPLv2 or DEATH
+Text Domain: ye_plugin
+ */
